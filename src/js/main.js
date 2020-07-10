ebf80be65bd752c340351e8dad5c1ff20b556b5f
Elena-Gallego <elenagallego.front@gmail.com>
2020-07-10 17:53:57 +0200

    add reload favourite list

--- src/js/main.js
+++ src/js/main.js
@@ -2,25 +2,63 @@
 
 //LOAD PAGE/
 //get data cache
-//JSON.stringify para convertir array a elemento string y poder
+//JSON.stringify para convertir array a elemento string y poderlo guardar en  localStorage.
+//REMOVE FAVORITE ITEM
+function removeFavoriteItem(event) {}
 
-//FUNCIÓN PARA RECARGAR LA LISTA DE FAVORITOS
+//REALOAD FAVOURITE LIST
 const favouriteList = document.querySelector(".js-favourite-list");
 function reloadFavouriteList(favouriteItems) {
   favouriteList.innerHTML = "";
   console.log(favouriteList);
-  for (let i = 0; i < favouriteList.length; i++) {
+  for (let i = 0; i < favouriteItems.length; i++) {
     const liElement = document.createElement("li");
     const imgElement = document.createElement("img");
+    imgElement.src = favouriteItems[i].image;
     liElement.appendChild(imgElement);
     const h2Element = document.createElement("h2");
-    h2Element.innerHTML = favouriteList[i].show.name;
+    h2Element.innerHTML = favouriteItems[i].name;
     liElement.appendChild(h2Element);
-    dataList.appendChild(liElement);
+    const buttonElement = document.createElement("button");
+    buttonElement.type = "button";
+    buttonElement.innerHTML = "x";
+    buttonElement.addEventListener("click", removeFavoriteItem);
+    liElement.appendChild(buttonElement);
+    favouriteList.appendChild(liElement);
   }
+  console.log(favouriteList);
+}
+//añadir botón con evento que haga splice y reload.
+
+//PAINT FAVOURITE LIST
+
+//array de objetos que tienen name y image como atributos
+
+let favourites = [];
+
+function selectFavourite(event) {
+  event.currentTarget.classList.toggle("favourite-styles");
+  const elemName = event.currentTarget.querySelector("h2").innerHTML;
+  const elemImg = event.currentTarget.querySelector("img").src;
+  const currentFavouriteIndex = favourites.findIndex((favourite) => {
+    return favourite.name === elemName;
+  });
+  console.log(currentFavouriteIndex);
+  if (currentFavouriteIndex === -1) {
+    const favouriteObject = {
+      name: elemName,
+      image: elemImg,
+    };
+    favourites.push(favouriteObject);
+    console.log(favourites);
+  } else {
+    favourites.splice(currentFavouriteIndex, 1);
+  }
+
+  reloadFavouriteList(favourites);
 }
 
-//LISTA QUE SE GENERA AL HACER CLICK EN EL BOTÓN SEARCH
+// GENERATE LISTDATA AFTER SEARCH
 
 const dataList = document.querySelector(".js-data-list");
 function getListData(event) {
@@ -43,13 +81,13 @@ function getListData(event) {
         h2Element.innerHTML = data[i].show.name;
         liElement.appendChild(h2Element);
         dataList.appendChild(liElement);
+        liElement.addEventListener("click", selectFavourite);
       }
     });
 }
 const buttonSearch = document.querySelector(".js-button-search");
 buttonSearch.addEventListener("click", getListData);
 
-//paint favourite list
 //listener object favourite list
 //paint reset button
 //listener event reset button
